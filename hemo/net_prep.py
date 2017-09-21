Many new networks saved. Some new scripts. Can numerically compute steady states without simulating full system for a fixed amount of time.

hemo/net_prep.py
@@ -23,8 +23,6 @@ def prep_net_for_sims(G):
         Graph Structure, ready to use for sims
     """
 
-
-
     set_pressures(G)
     set_inverse_transit_times(G)
     redirect(G)
@@ -121,7 +119,7 @@ def set_pressures(G):
     """Ensures that all nodes have their pressure computed.
 
     Each edge already has attributes 'length', 'radius', 'capacitance', 'mat_idx' assigned to them.
-    Using Hagen-Pousielle equation and Kirchoff's current law, calculates pressure at each internal node.
+    Using Hagen-Pousielle equation and Kirchoff'mean_steady_states current law, calculates pressure at each internal node.
 
     Parameters
     ----------
@@ -155,7 +153,7 @@ def set_pressures(G):
         G.graph['n_internal'] = m_iter
 
     assign_matrix_indices(G)
-    viscosity = 3.5
+    viscosity = 3.5e-3
 
     def set_capacitance(g):
         for src2, sink2 in g.edges():
@@ -191,9 +189,9 @@ def set_pressures(G):
         if G.node[node1]['ntype'] == 'internal':
             for otherNode in G.nodes():
                 if G.node[otherNode]['ntype'] == 'source' and G.has_edge(otherNode, node1):
-                    b[G.node[node1]['mat_idx']] = p0 * G[otherNode][node1]['capacitance']
+                    b[G.node[node1]['mat_idx']] += p0 * G[otherNode][node1]['capacitance']
                 elif G.node[otherNode]['ntype'] == 'sink' and G.has_edge(node1, otherNode):
-                    b[G.node[node1]['mat_idx']] = pN * G[node1][otherNode]['capacitance']
+                    b[G.node[node1]['mat_idx']] += pN * G[node1][otherNode]['capacitance']
     # print(b)
 
     p = np.linalg.solve(A, b)
@@ -262,7 +260,7 @@ def set_inverse_transit_times(G):
     -------
 
     """
-    viscosity = 3.5
+    viscosity = 3.5e-3
     for src, sink in G.edges():
         dp = G.node[src]['pressure'] - G.node[sink]['pressure']
         G[src][sink]['inverse_transit_time'] = dp * (G[src][sink]['radius'] ** 2) / (
@@ -317,7 +315,7 @@ def plot_3d_network(G, title=None, filename=None):
         xs = [G.node[src]['pos'][0], G.node[sink]['pos'][0]]
         ys = [G.node[src]['pos'][1], G.node[sink]['pos'][1]]
         zs = [G.node[src]['pos'][2], G.node[sink]['pos'][2]]
-        ax.plot3D(xs, ys, zs, 'r')
+        ax.plot3D(xs, ys, zs, 'mean_radii')
 
     if title is None:
         ax.set_title('Network, N=%i' % G.graph['N'])

hemo/sims.py
@@ -2,7 +2,7 @@ import numpy as np
 from string import Template
 
 
-def create_source(G, *, v0=5, B=65, k2=0.35, K=34.2, k3=5.0e-4, k4=0, filename="system.py"):
+def create_source(G, *, v0=5, B=30, k2=0.35, K=34.2, k3=5.0e-4, k4=0, filename="system.py"):
     """Creates a system source file.
 
 
@@ -15,7 +15,7 @@ def create_source(G, *, v0=5, B=65, k2=0.35, K=34.2, k3=5.0e-4, k4=0, filename="
     v0 : float
         The upstream concentration
     B : float
-        Vascular volume ratio (default 65)
+        Vascular volume ratio (default 30)
     k2 : float
         Maximal transport rate (default 0.35)
     K : float
@@ -48,7 +48,7 @@ def create_source(G, *, v0=5, B=65, k2=0.35, K=34.2, k3=5.0e-4, k4=0, filename="
             dv = "    dv[%d] = -%.18f*v[%d] - (%.18f * v[%d])/(%.18f + v[%d])" % (
             id, f, id, parms['k2'], id, parms['K'], id)
             dw = "    dw[%d] = + (%.18f * v[%d])/(%.18f + v[%d]) - %.18f*w[%d]" % (
-            id, parms['k2'] / parms['B'], id, parms['K'], id, parms['k3'], id)
+            id, parms['k2'] / parms['B'], id, parms['K'], id, parms['B'] *parms['k3'], id)
 
             if G.node[src]['ntype'] == 'source':
                 dv += " + %.18f * np.exp(-%.18f * t)" % (f * parms['v0'], parms['k4'])

scripts/scaling_net.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import system
 import importlib
+import time
 
 
-viscosity = 3.5
+viscosity = 3.5e-3
 
 
 def total_resistance(G):
@@ -138,9 +139,8 @@ def central_difference(G, edge):
 
 
 
-def create_network_multiple_sources_and_sinks(N, *, space_volume=1, vascular_fraction=0.015, radii_iters=2):
+def create_network_multiple_sources_and_sinks(N, *, space_volume=1, vascular_fraction=0.015, radii_iters=2, symmetric=False):
     delta = 1 / (N + 1)
-    viscosity = 3.5
 
     G = nx.DiGraph()
     G.graph['N'] = N
@@ -187,38 +187,30 @@ def create_network_multiple_sources_and_sinks(N, *, space_volume=1, vascular_fra
     # assign lengths
     net_prep.calculate_lengths(G)
 
-    assign_distances_from_source_and_sink_nodes(G)
+    if not symmetric:
+        assign_distances_from_source_and_sink_nodes(G)
+        def scale_parm(n):
+            return (5 * (n+1) * np.sqrt(90 * np.pi))**-1
 
-    # # assign initial radius uniformly
-    # total_len = np.sum([G[src][sink]['length'] for src, sink in G.edges()])
-    # r = np.sqrt((vascular_fraction * space_volume) / (np.pi * total_len))
-    # for src, sink in G.edges():
-    #     G[src][sink]['radius'] = r
-    #
-    # # perturb radii to further break symmetry
-    # for j in range(radii_iters):
-    #     for i in range(len(G.edges())):
-    #         perturb_radii(G, i)
-    #         # net_prep.set_volumes(G)
-    # # make_switches(G)
+        alpha = 5
+        beta = scale_parm(N)
 
-    def scale_parm(n):
-        return (5 * (n+1) * np.sqrt(90 * np.pi)/(10**4))**-1
+        for src, sink in G.edges():
+            G[src][sink]['radius'] = np.random.gamma(alpha, beta)
 
-    alpha = 5
-    beta = scale_parm(N)
-    for src, sink in G.edges():
-        G[src][sink]['radius'] = 10**-4 * np.random.gamma(alpha, beta)
+        for _ in range(radii_iters):
+            for src1, sink1 in G.edges():
+                for src2, sink2 in G.edges():
+                    if src1 == src2:
+                        continue
+                    if G[src1][sink1]['center_dist'] > G[src2][sink2]['center_dist'] and G[src1][sink1]['radius'] < G[src2][sink2]['radius']:
+                        temp_radius = G[src1][sink1]['radius']
+                        G[src1][sink1]['radius'] = G[src2][sink2]['radius']
+                        G[src2][sink2]['radius'] = temp_radius
 
-    for _ in range(radii_iters):
-        for src1, sink1 in G.edges():
-            for src2, sink2 in G.edges():
-                if src1 == src2:
-                    continue
-                if G[src1][sink1]['center_dist'] > G[src2][sink2]['center_dist'] and G[src1][sink1]['radius'] < G[src2][sink2]['radius']:
-                    temp_radius = G[src1][sink1]['radius']
-                    G[src1][sink1]['radius'] = G[src2][sink2]['radius']
-                    G[src2][sink2]['radius'] = temp_radius
+    elif symmetric:
+        for src, sink in G.edges():
+            G[src][sink]['radius'] = 1/((N+1) * np.sqrt(90*np.pi))
 
     net_prep.prep_net_for_sims(G)
 
@@ -321,10 +313,25 @@ def run_example_sim(n):
 
 
 if __name__ == '__main__':
-    for n in [4,5,6,7,8,9,10]:
+    # for k in [11,12,13,14,15]:
+    #     for n in [4,5,6,7,8,9,10]:
+    #         print('Creating G_%i #%i' % (n, k))
+    #         G = create_network_multiple_sources_and_sinks(n)
+    #         nx.write_gpickle(G, 'G_%i_%i.gpickle' % (n, k))
+    #         # radii = [G[src][sink]['radius'] for src, sink in G.edges()]
+    #         # plt.hist(radii)
+    #         # plt.title('Radii for G_%i' % n)
+    #         # plt.show()
+
+
+    for n in [4,5,6,7,8,9,10,11]:
+        k = 0
+        start = time.time()
         G = create_network_multiple_sources_and_sinks(n)
-        nx.write_gpickle(G, 'G_%i.gpickle' % n)
-        radii = [G[src][sink]['radius'] for src, sink in G.edges()]
-        plt.hist(radii)
-        plt.title('Radii for G_%i' % n)
-        plt.show()
+        end = time.time()
+        print('Created %i - %i in %.01f seconds.' % (n, k, end-start))
+        nx.write_gpickle(G, 'C:/Users/Bill/Documents/Python/hemo/hemo/data/networks/G_%i_%i.gpickle' % (n, k))
+    # radii = [G[src][sink]['radius'] for src, sink in G.edges()]
+    # plt.hist(radii)
+    # plt.title('Radii for G_%i' % n)
+    # plt.show()